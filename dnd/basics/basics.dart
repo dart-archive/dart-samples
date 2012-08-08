56707f92aacb35b298c2a39f795c2deaecc4f550
Jason Brooks <jason.brooks@gmail.com>
2012-08-07 20:45:29 -0400

    Add DnD Basics sample; top-level COPYING file.

---
+++ dnd/basics/basics.dart
@@ -0,0 +1,72 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the COPYING file.
+
+#import('dart:html');
+
+class Basics {
+  Element _dragSourceEl;
+
+  void start() {
+    var cols = document.queryAll('#columns .column');
+    for (var col in cols) {
+      col.on.dragStart.add(_onDragStart);
+      col.on.dragEnd.add(_onDragEnd);
+      col.on.dragEnter.add(_onDragEnter);
+      col.on.dragOver.add(_onDragOver);
+      col.on.dragLeave.add(_onDragLeave);
+      col.on.drop.add(_onDrop);
+    }
+  }
+
+  void _onDragStart(MouseEvent event) {
+    Element dragTarget = event.target;
+    dragTarget.classes.add('moving');
+    _dragSourceEl = dragTarget;
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', dragTarget.innerHTML);
+  }
+
+  void _onDragEnd(MouseEvent event) {
+    Element dragTarget = event.target;
+    dragTarget.classes.remove('moving');
+    var cols = document.queryAll('#columns .column');
+    for (var col in cols) {
+      col.classes.remove('over');
+    }
+  }
+
+  void _onDragEnter(MouseEvent event) {
+    Element dropTarget = event.target;
+    dropTarget.classes.add('over');
+  }
+
+  void _onDragOver(MouseEvent event) {
+    // This is necessary to allow us to drop.
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }
+
+  void _onDragLeave(MouseEvent event) {
+    Element dropTarget = event.target;
+    dropTarget.classes.remove('over');
+  }
+
+  void _onDrop(MouseEvent event) {
+    // Stop the browser from redirecting.
+    event.stopPropagation();
+
+    // Don't do anything if dropping onto the same column we're dragging.
+    Element dropTarget = event.target;
+    if (_dragSourceEl != dropTarget) {
+      // Set the source column's HTML to the HTML of the column we dropped on.
+      _dragSourceEl.innerHTML = dropTarget.innerHTML;
+      dropTarget.innerHTML = event.dataTransfer.getData('text/html');
+    }
+  }
+}
+
+void main() {
+  var basics = new Basics();
+  basics.start();
+}
