0ffe220281e31834873e543cf3eace66b5d1fbdb
Richard Griffith <scribe.griff@gmail.com>
2012-10-01 20:18:11 -0500

    added sharpen and blur filters

--- canvas/imagefilters/imagefilters.dart
+++ canvas/imagefilters/imagefilters.dart
@@ -7,6 +7,7 @@
 
 
 #import('dart:html');
+#import('dart:math');
 
 class Filters {
   ImageData pixels;
@@ -22,6 +23,7 @@ class Filters {
     return context.getImageData(0, 0, canvas.width, canvas.height);
   }
 
+  // Apply grayscale filter.
   ImageData grayscale() {
     var d = pixels.data;
     for (var i = 0; i < d.length; i += 4) {
@@ -35,6 +37,7 @@ class Filters {
     return pixels;
   }
 
+  // Apply brightness filter.
   ImageData brightness(int brightAdj) {
     var d = pixels.data;
     for (var i=0; i<d.length; i+=4) {
@@ -45,6 +48,7 @@ class Filters {
     return pixels;
   }
 
+  // Apply threshold filter.
   ImageData threshold(int thresholdVal) {
     var d = pixels.data;
     for (var i = 0; i < d.length; i += 4) {
@@ -56,12 +60,64 @@ class Filters {
     }
     return pixels;
   }
+
+  // Apply convolution filter.
+  ImageData convolve(List weights, [bool opaque = false]) {
+    var alphaFac = opaque ? 1 : 0;
+    var side = (sqrt(weights.length).toInt());
+    var halfSide = (side / 2).toInt();
+
+    var d = pixels.data;
+    var width = pixels.width;
+    var height = pixels.height;
+
+    //Create a temporary canvas to store the filtered data.
+    CanvasElement tempCanvas = new CanvasElement(width, height);
+    CanvasRenderingContext2D tempContext = tempCanvas.getContext('2d');
+    ImageData output = tempContext.createImageData(width, height);
+    var dest = output.data;
+
+    //Loop over the image.
+    for (var y = 0; y < height; y++) {
+      for (var x = 0; x < width; x++) {
+        var r = 0.0, g = 0.0, b = 0.0, a = 0.0;
+        var destOff = (y * width + x) * 4;
+        //Now loop over the filter mask.
+        for (var fy = 0; fy < side; fy++) {
+          for (var fx = 0; fx < side; fx++) {
+            var srcy = min(height - 1, max(0, y + fy - halfSide));
+            var srcx = min(width - 1, max(0, x + fx - halfSide));
+            var srcOff = (srcy * width + srcx) * 4;
+            var weight = weights[(fy * side + fx)];
+            r += d[srcOff] * weight;
+            g += d[srcOff + 1] * weight;
+            b += d[srcOff + 2] * weight;
+            a += d[srcOff + 3] * weight;
+          }
+        }
+        dest[destOff] = r.toInt();
+        dest[destOff + 1] = g.toInt();
+        dest[destOff + 2] = b.toInt();
+        dest[destOff + 3] = (a + alphaFac * (255 - a)).toInt();
+      }
+    }
+    return output;
+  }
 }
 
 void main() {
 
   final brightAdj = 40;
   final thresholdVal = 128;
+
+  List sharpenMask = new List();
+  List blurMask = new List();
+  sharpenMask = [0, -1,  0,
+                 -1,  5, -1,
+                 0, -1,  0];
+  blurMask = [1/9, 1/9, 1/9,
+              1/9, 1/9, 1/9,
+              1/9, 1/9, 1/9];
 
   ImageElement img = query('.orig');
   window.on.load.add((e) => populateImages(img));
@@ -93,6 +149,26 @@ void main() {
     } else {
       ImageData pixels = new Filters(img).threshold(thresholdVal);
       filterImage('threshold', pixels);
+    }
+  });
+
+  // Click listener for sharpen.
+  document.query('[name = "sharpen"]').on.click.add((e) {
+    if (query('#sharpen').previousElementSibling.style.display == 'none') {
+      restoreContent('sharpen');
+    } else {
+      ImageData pixels = new Filters(img).convolve(sharpenMask);
+      filterImage('sharpen', pixels);
+    }
+  });
+
+  // Click listener for blur.
+  document.query('[name = "blur"]').on.click.add((e) {
+    if (query('#blur').previousElementSibling.style.display == 'none') {
+      restoreContent('blur');
+    } else {
+      ImageData pixels = new Filters(img).convolve(blurMask);
+      filterImage('blur', pixels);
     }
   });
 }
