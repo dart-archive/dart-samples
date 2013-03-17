b7847afe7134316b5220b725c5a15bc791b27c8c
Shailen Tuli <shailen.tuli@gmail.com>
2013-03-17 12:05:35 -0700

    Added script to format book.asciidoc for dartlang.org
    Temporary changes to pubspec.
    Whitespace cleanup in book.asciidoc to ensure smooth conversion to
    format needed by dartlang.org.

---
+++ format_for_dartlang.dart
@@ -0,0 +1,143 @@
+#!/usr/bin/env dart
+
+import 'dart:io';
+import 'dart:async';
+
+var content = '';
+bool _headersConverted = false;
+
+var preface = '''---
+layout: default
+title: "Dart Cookbook"
+description: "Tasty recipes to make you more productive with Dart."
+---
+
+# Dart Cookbook
+
+## Contents
+''';
+
+/// Removes the asciidoc header.
+void stripPreface() {
+  // Strip preface.
+  var regExp = new RegExp(r'= Dart Cookbook\n:author: Shailen Tuli\n:encoding: UTF-8');
+  content = content.replaceFirst(regExp, '');
+}
+
+/// Converts asciidoc === tags to equivalent markdown ### tags.
+void convertHeaders() {  
+  var regExp = new RegExp('\n(={1,6}) ');
+  content = content.replaceAllMapped(regExp, (match) {
+    var oldTag = match.group(1);
+    var newTagList = [];
+    for (var i = 0; i < oldTag.length; i++) {
+      newTagList.add("#");
+    }
+    return "\n${newTagList.join('')} ";
+  });
+  _headersConverted = true;
+}
+
+String getHref(title) {
+  return title.trim().toLowerCase().replaceAll(' ', '_');  
+}
+
+/// Generates TOC.
+String generateTOC() {
+  if (!_headersConverted) {
+    throw new StateError('cannot generate TOC until convertHeaders() has been'
+        'called');
+  }
+  var sb = new StringBuffer();
+  var regExp = new RegExp(r'(^#{1,3}) ([\w\s]+)\n', multiLine: true);
+  var matches = regExp.allMatches(content);
+  var closingTags = new List<String>();
+  
+  var h2ClosingTags = [];
+  bool firstH2 = true;
+  
+  // The <ol> for the TOC.
+  sb.write('<ol>\n');
+  
+  matches.forEach((match) {
+    var hType = match.group(1);
+    var text = match.group(2).trim();
+    if (hType == "##") {
+      if (!h2ClosingTags.isEmpty) {
+        sb.write(h2ClosingTags.removeLast());
+      }
+      sb.write('<li><a href="#${getHref(text)}">$text</a><ol>\n');
+      h2ClosingTags.addAll(['</li>\n', '</ol>\n']);
+    } else if (hType == "###") {
+      sb.write('<li><a href="${getHref(text)}">$text</a></li>\n');      
+    }
+  });
+  
+  // Final H2 closing tag (only H2's nest).
+  sb.write('\n</ol>\n<li>');
+
+  // Add final TOC closing tag.
+  sb.write('\n</ol>'); 
+  return sb.toString();
+}
+
+
+/// Makes all h3 elements named anchors.
+void generateH3Anchors() {
+  if (!_headersConverted) {
+    throw new StateError('cannot generate H3 anchors until convertHeaders() has'
+      'been called');
+  }
+  var regExp = new RegExp(r'(\n### )([[A-Z][\w\s]+)\n');
+  content = content.replaceAllMapped(regExp, (match) {
+    var anchorId = match.group(2).toLowerCase().replaceAll(' ', '_').trim();
+    return('\n<h3><a id="${anchorId}" href="#${anchorId}">${match.group(2).trim()}</a></h3>\n\n');  
+  });
+}
+
+
+/// Converts asciidoc code delimiters ('-------') to <pre> tags.
+void generatePreTags() {
+  if (!_headersConverted) {
+    throw new StateError('cannot generate pre tags until convertHeaders() has'
+      'been called');
+  }
+  var regExp = new RegExp('\n\n-+\n');
+  var matches = regExp.allMatches(content);
+  content = content.replaceAll(regExp, '\n\n<pre class="programlisting">\n');
+  
+  regExp = new RegExp('\n-+\n\n');
+  matches = regExp.allMatches(content); 
+  content = content.replaceAll(regExp, '\n<pre class="programlisting">\n\n');
+}
+
+
+/// Writes generated content to out file.
+void writeToOutputFile(File outFile, preface, toc) {
+  outFile.writeAsStringSync(preface);
+  outFile.writeAsStringSync(toc, mode: FileMode.APPEND);
+  outFile.writeAsStringSync(content, mode: FileMode.APPEND);
+}
+
+
+void main() {  
+  var args = new Options().arguments;
+  if (args.length != 1) {
+    print("Usage: format_for_dartlang.dart newfile");
+    exit(1);
+  }
+  
+  var outFile = new File(args[0]);
+  var sourceFile = 'book.asciidoc';
+  var file = new File(sourceFile);
+ 
+  content = file.readAsStringSync(); 
+  stripPreface();
+  convertHeaders();
+  
+  var toc = generateTOC();
+  generateH3Anchors();
+  generatePreTags();
+
+  writeToOutputFile(outFile, preface, toc);
+}
