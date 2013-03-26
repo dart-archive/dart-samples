aae109b8be272d870e3343a7027f31e4a4c57d42
Shailen Tuli <shailen.tuli@gmail.com>
2013-03-25 19:43:35 -0700

    Wrote number to string recipe

--- recipes/test/core/numbers/converting_a_number_to_a_string_test.dart
+++ recipes/test/core/numbers/converting_a_number_to_a_string_test.dart
@@ -10,8 +10,7 @@ void main() {
     expect(1234.toStringAsExponential(), equals('1.234e+3'));
     expect(3.1519.toStringAsExponential(), equals('3.1519e+0'));
     
-    // You can specify the fraction digits.
-    expect(1234.toStringAsExponential(2), equals('1.23e+3'));
+    // You can specify the digits after the decimal point.
     expect(3.1519.toStringAsExponential(5), equals('3.15190e+0'));
   });
   
@@ -21,7 +20,7 @@ void main() {
   });
   
   test('convert the number with n significant digits', () {
-    expect(1234.toStringAsPrecision(6), equals('1234.00'));
-    expect(3.1519.toStringAsPrecision(3), equals('3.15'));
+    expect(1234.toStringAsPrecision(5), equals('1234.0'));
+    expect(3.1519.toStringAsPrecision(8), equals('3.1519000'));
   });
 }
