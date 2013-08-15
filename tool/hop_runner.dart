bdd435612c700a1928f97b411966c05fd7c03aea
Shailen Tuli <shailen.tuli@gmail.com>
2013-08-14 21:33:58 -0700

    Updated hop_runner and updated pubspec dependencies
    R=financeCoding@gmail.com
    
    Review URL: https://chromiumcodereview.appspot.com//23203005

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,42 +1,45 @@
 library hop_runner;
 
-import 'dart:async';
 import 'dart:io';
+import 'package:path/path.dart' as path;
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
 
+import 'package:html5lib/parser.dart' show parse;
+import 'package:html5lib/dom.dart';
+
+Iterable<String> getHtmlPaths() {
+  return new Directory('web').listSync(recursive: true, followLinks: false)
+      .map((File file) => file.path)
+      .where((_path) => path.extension(_path) == ".html");
+}
+
+List<String> getDartPaths(htmlPaths) {
+  List<String> paths = [];
+  for (var htmlPath in htmlPaths) {
+    var file = new File(htmlPath).readAsStringSync();
+    var contents = parse(file);
+    var scripts = contents.body.queryAll("script");
+    if (!scripts.isEmpty) {
+      for(var script in scripts) {
+        if (script.attributes['type'] == 'application/dart') {
+          // Links to .dart files may not always be in the same directory as
+          // the .html file. The following line computes the correct path of
+          // the .dart file.
+          // So, given a web/app/index.html file that contains a
+          // <script type='application/dart' src=../../foo.dart'> tag, the .dart
+          // file is correctly identified as web/foo.dart.
+          paths.add(path.normalize(path.join(
+              path.dirname(htmlPath), script.attributes['src'])));
+        }
+      }
+    }
+  }
+  return paths;
+}
+
 void main() {
-
-  var samples = [ 'web/appcache/beginner/appcache.dart',
-                  'web/canvas/hidpi/hidpi.dart',
-                  'web/canvas/imagefilters/imagefilters.dart',
-                  'web/dnd/basics/basics.dart',
-                  'web/file/dndfiles/dndfiles.dart',
-                  'web/file/dndfiles/monitoring.dart',
-                  'web/file/dndfiles/slicing.dart',
-                  'web/file/filesystem/filesystem.dart',
-                  'web/file/terminal/terminal_filesystem.dart',
-                  'web/geolocation/trip_meter/trip_meter.dart',
-                  'web/indexeddb/todo/todo.dart',
-                  'web/localstorage/basics/localstorage.dart',
-                  'web/notifications/quick/notifications_sample.dart',
-                  'web/pointerlock/fps/fps.dart',
-                  'web/speed/animations/animations.dart',
-                  'web/video/basics/video.dart',
-                  'web/webaudio/intro/filter_sample.dart',
-                  'web/webgl/2d_image/2d_image.dart',
-                  'web/webgl/2d_image_3x3_convolution/2d_image_3x3_convolution.dart',
-                  'web/webgl/2d_image_blend/2d_image_blend.dart',
-                  'web/webgl/2d_image_processing/2d_image_processing.dart',
-                  'web/webgl/2d_image_red_2_blue/2d_image_red_2_blue.dart',
-                  'web/webgl/fundamentals/fundamentals.dart',
-                  'web/webgl/rectangle/rectangle.dart',
-                  'web/webgl/rectangles/rectangles.dart',
-                  'web/webgl/rectangle_top_left/rectangle_top_left.dart',
-                  'web/webgl/utils/webgl_utils.dart',
-                  'web/websockets/basics/websocket_sample.dart' ];
-
-  addTask('analyze_web', createDartAnalyzerTask(samples));
-
+  var paths = getDartPaths(getHtmlPaths());
+  addTask('analyze_libs', createAnalyzerTask(paths));
   runHop();
 }
