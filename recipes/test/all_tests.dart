d668d24ddfcc303b00cdc08a6d7d5d0d507f4e74
Shailen Tuli <shailentuli@shailentuli-macbookpro2.roam.corp.google.com>
2013-11-17 20:40:18 -0800

    Cleaned up JSON tests

--- recipes/test/all_tests.dart
+++ recipes/test/all_tests.dart
@@ -1,7 +1,9 @@
 import "strings_test.dart" as strings_test;
 import "lists_test.dart" as lists_test;
+import "json_test.dart" as json_test;
 
 void main() {
   strings_test.main();
   lists_test.main();
+  json_test.main();
 }
--- recipes/test/json_test.dart
+++ recipes/test/json_test.dart
@@ -1,10 +1,11 @@
-import 'dart:json' as json;
+
+import 'dart:convert' show JSON;
 import 'package:unittest/unittest.dart';
 
 class Person {
   String name;
   num age;
-  
+
   Person(this.name, this.age);
   dynamic toJson() => {"name": name, "age": age};
 }
@@ -12,42 +13,66 @@ class Person {
 class Book {
   String title;
   num numPages;
- 
+
   Book(this.title, this.numPages);
 }
 
 main() {
   group('test stringify()', () {
-    var person = {'name': 'joe', 
+    var person = {'name': 'joe',
                   'born':  2002,
                   'into': {'films' : ['crime', 'noir']},
                   'aliases': []};
-    
+
     test('simple', () {
-      expect(json.stringify(person) == '{"name":"joe","born":2002,"into":{"films":["crime","noir"]},"aliases":[]}', isTrue);
+      expect(JSON.encode(person) == '{"name":"joe","born":2002,"into":{"films":["crime","noir"]},"aliases":[]}', isTrue);
     });
-    
+
     test('with object with a toJson() defined', () {
       var person = new Person('john', 32);
       // JSON only deals with double quotes.
-      expect(json.stringify(person), equals('{"name":"john","age":32}'));
+      expect(JSON.encode(person), equals('{"name":"john","age":32}'));
     });
-    
+
     test('with object lacking toJson()', () {
       var person = new Person('john', 32);
       var book = new Book('War and Peace', 1089);
       var object = {'person': person, 'reads' : book};
-      
-      expect(() => json.stringify(book), 
-          throwsA(predicate((e) => e is json.JsonUnsupportedObjectError)));
-      
+
+      expect(() => JSON.encode(book), throws);
+    
       try {
-        json.stringify(object);
+        JSON.encode(object);
         throw "managed to stringify an object without a toJson()";
       } catch(e) {
         expect(e.cause.toString().startsWith(
           "Class 'Book' has no instance method 'toJson'."), isTrue);
       }
     });
-  });  
+  });
+
+
+  group('JSON.decode()', () {
+
+    var jsonPerson = '{"name" : "joe", "date" : [2013, 3, 10]}';
+
+    test('simple parse', () {
+      var person = JSON.decode(jsonPerson);
+      expect(person['name'], equals('joe'));
+      expect(person['date'], equals([2013, 3, 10]));
+      expect(person['date'] is List, isTrue);
+    });
+
+    test('with reviver', () {
+      var person = JSON.decode(jsonPerson, reviver: (key, value) {
+        if (key == "date") {
+          return new DateTime(value[0], value[1], value[2]);
+        }
+        return value;
+      });
+
+      expect(person['name'], equals('joe'));
+      expect(person['date'] is DateTime, isTrue);
+    });
+  });
 }
