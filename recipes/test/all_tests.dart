d3187e5e44fc032c7a82435c0d258776862b3c3e
Shailen Tuli <shailentuli@shailentuli-macbookpro2.roam.corp.google.com>
2013-11-17 21:45:05 -0800

    Merged uri tests

--- recipes/test/all_tests.dart
+++ recipes/test/all_tests.dart
@@ -2,10 +2,12 @@ import "strings_test.dart" as strings_test;
 import "lists_test.dart" as lists_test;
 import "json_test.dart" as json_test;
 import "numbers_test.dart" as numbers_test;
+import "uri_test.dart" as uri_test;
 
 void main() {
   strings_test.main();
   lists_test.main();
   json_test.main();
   numbers_test.main();
+  uri_test.main();
 }
