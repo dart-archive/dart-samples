db6e578df7e5fe30b7eb9d354651384b9dea002d
John J. Yates <JohnJYates@gmail.com>
2014-03-29 12:57:54 -0400

    fix bug from issue #79
    https://developer.mozilla.org/en-US/docs/Web/API/MouseWheelEvent
    The method "wheelDeltaX" returns an int but the above class is only
    supported by Chrome/Safari and is deprecated so we must use:
    https://developer.mozilla.org/en-US/docs/Web/API/WheelEvent
    Which returns all doubles.  The least impact is incurred by simply
    rounding, which achieves the same behavior as "wheelDeltaX".

--- html5/web/webgl/solar3d/web/solar.dart
+++ html5/web/webgl/solar3d/web/solar.dart
@@ -185,8 +185,8 @@ class Solar3DApplication {
     if (event is WheelEvent) {
       WheelEvent e = event;
       controller.mouseSensitivity = 720.0;
-      controller.accumDX -= e.deltaX;
-      controller.accumDY += e.deltaY;
+      controller.accumDX -= e.deltaX.round();
+      controller.accumDY += e.deltaY.round();
     }
     event.preventDefault();
   }
